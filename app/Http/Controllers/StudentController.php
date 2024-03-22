f0b325642b020b80d962afa7d2e5c28eb1c181b7
Nuraeni <nuraeniexecutive18@gmail.com>
2024-03-23 06:08:22 +0800

    add validation

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
@@ -18,6 +19,26 @@ class StudentController extends Controller
         $phone = $request->input('phone');
 
         foreach ($name as $key => $value) {
+            $validator = Validator::make(
+                [
+                    'name' => $request->input('name')[$key],
+                    'address' => $request->input('address')[$key],
+                    'phone' => $request->input('phone')[$key],
+                ],
+                [
+                    'name' => ['required', 'string', 'max:255'],
+                    'address' => ['required', 'string', 'max:255'],
+                    'phone' => ['required', 'numeric', 'unique:students'],
+                ],
+                [
+                    'phone.unique' => 'No HP ' . $request->input('phone')[$key] . ' sudah digunakan.',
+                ],
+            );
+            // Cek jika validasi gagal
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
             $data[] = [
                 'name' => $name[$key],
                 'nim' => $nim[$key],
@@ -30,7 +51,7 @@ class StudentController extends Controller
             ];
         }
 
-        // Lakukan bulk insert
+        //bulk insert students
         Student::insert($data);
         return view('index')->with('success', 'Data mahasiswa berhasil disimpan.');
     }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,6 +8,10 @@
         padding-top: 10px;
         /* Optional padding for spacing between forms */
     }
+
+    .error-message {
+        color: red;
+    }
 </style>
 
 <form action="{{ route('student.store') }}" method="post" style="margin:50px" id="dynamicForm">
@@ -22,76 +26,99 @@
         </button>
     </div>
     <hr>
+    @if ($errors->any())
+        <div class="error-message">
+
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <input type="hidden" name="form_counter" value="{{ old('form_counter', 1) }}">
+
     <div class="form-group">
         <label>Nama</label>
-        <input class="form-control" type="text" placeholder="Nama..." name="name[]">
+        <input class="form-control" type="text" placeholder="Nama..." name="name[]" value="{{ old('name.0') }}"
+            required>
     </div>
     <div class="form-group">
         <label>NIM</label>
-        <input class="form-control" type="number" placeholder="NIM..." name="nim[]">
+        <input class="form-control" type="number" placeholder="NIM..." name="nim[]" value="{{ old('nim.0') }}"
+            required>
     </div>
     <div class="form-group">
         <label for="jenis_kelamin">Jenis Kelamin</label>
-        <select class="form-control" id="jenis_kelamin" name="gender[]">
-            <option value="laki-laki">Laki-laki</option>
-            <option value="perempuan">Perempuan</option>
+        <select class="form-control" id="jenis_kelamin" name="gender[]" required>
+            <option value="laki-laki" @if (old('gender.0') == 'laki-laki') selected @endif>Laki-laki</option>
+            <option value="perempuan" @if (old('gender.0') == 'perempuan') selected @endif>Perempuan</option>
         </select>
     </div>
     <div class="form-group">
         <label>Jurusan</label>
-        <input class="form-control" type="text" placeholder="Jurusan..." name="major[]">
+        <input class="form-control" type="text" placeholder="Jurusan..." name="major[]" value="{{ old('major.0') }}"
+            required>
     </div>
-
     <div class="form-group">
         <label>No Hanphone</label>
-        <input class="form-control" type="number" placeholder="No hp..." name="phone[]">
+        <input class="form-control" type="number" placeholder="No hp..." name="phone[]" value="{{ old('phone.0') }}"
+            required>
     </div>
     <div class="form-group">
         <label>Alamat</label>
-        <input class="form-control" type="text" placeholder="Alamat..." name="address[]">
+        <input class="form-control" type="text" placeholder="Alamat..." name="address[]"
+            value="{{ old('address.0') }}" required>
     </div>
 
 </form>
 
 
 <div id="formTemplate" style="display: none;">
+
+
     <div class="form-group">
         <label>Nama</label>
-        <input class="form-control" type="text" placeholder="Nama..." name="name[]">
+        <input class="form-control" type="text" placeholder="Nama..." name="name[]" value="{{ old('name.*') }}"
+            required>
     </div>
     <div class="form-group">
         <label>NIM</label>
-        <input class="form-control" type="number" placeholder="NIM..." name="nim[]">
+        <input class="form-control" type="number" placeholder="NIM..." name="nim[]" value="{{ old('nim.*') }}"
+            required>
     </div>
     <div class="form-group">
         <label for="jenis_kelamin">Jenis Kelamin</label>
-        <select class="form-control" id="jenis_kelamin" name="gender[]">
-            <option value="laki-laki">Laki-laki</option>
-            <option value="perempuan">Perempuan</option>
+        <select class="form-control" id="jenis_kelamin" name="gender[]" required>
+            <option value="laki-laki" {{ old('gender.*') == 'laki-laki' ? 'selected' : '' }}>Laki-laki</option>
+            <option value="perempuan" {{ old('gender.*') == 'perempuan' ? 'selected' : '' }}>Perempuan</option>
         </select>
     </div>
     <div class="form-group">
         <label>Jurusan</label>
-        <input class="form-control" type="text" placeholder="Jurusan..." name="major[]">
+        <input class="form-control" type="text" placeholder="Jurusan..." name="major[]" value="{{ old('major.*') }}"
+            required>
     </div>
 
     <div class="form-group">
         <label>No Hanphone</label>
-        <input class="form-control" type="number" placeholder="No hp..." name="phone[]">
+        <input class="form-control" type="number" placeholder="No hp..." name="phone[]" value="{{ old('phone.*') }}"
+            required>
     </div>
     <div class="form-group">
         <label>Alamat</label>
-        <input class="form-control" type="text" placeholder="Alamat..." name="address[]">
+        <input class="form-control" type="text" placeholder="Alamat..." name="address[]"
+            value="{{ old('address.*') }}" required>
     </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function() {
         // Counter to track the number of added forms
-        let formCounter = 1;
+        let formCounter = parseInt("{{ old('form_counter', 1) }}");
 
         // Add Form button click event
-        $('#addForm').on('click', function() {
+        function addClonedForm() {
             // Clone the form template
             const clonedForm = $('#formTemplate').clone().removeAttr('id').addClass('clone-form');
 
@@ -108,13 +135,19 @@
             // Show the cloned form
             clonedForm.show();
 
-            // Increment the form counter
+            // Increment the formCounter value
             formCounter++;
 
             // Show Kurang Form button when there is more than one form
             if (formCounter > 1) {
                 $('#removeForm').show();
             }
+        }
+        $('#addForm').on('click', function() {
+            addClonedForm();
+
+            // Simpan nilai form_counter ke dalam input hidden
+            $('input[name="form_counter"]').val(formCounter);
         });
 
         // Kurang Form button click event
@@ -129,6 +162,10 @@
             if (formCounter === 1) {
                 $('#removeForm').hide();
             }
+            $('input[name="form_counter"]').val(formCounter);
         });
+        for (let i = 1; i < formCounter; i++) {
+            addClonedForm();
+        }
     });
 </script>
